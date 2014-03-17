63ae1cbbf6748409bb9f498387823c84c2366364
Paavo <pparkkin@gmail.com>
2014-03-17 21:57:44 +0900

    Clean up ship info queries.

--- src/viewer/model/ship.clj
+++ src/viewer/model/ship.clj
@@ -5,14 +5,16 @@
               "postgresql://localhost:5432/evedata"))
 
 (defn query-ship-info [typeid]
-  (first (sql/query spec
-                    [(format "select * from \"invTypes\" where \"typeID\" = %d" typeid)])))
+  (cond (instance? Number typeid)
+        (first (sql/query spec
+                          [(format "select * from \"invTypes\" where \"typeID\" = %d" typeid)]))
+        (instance? String typeid)
+        (first (sql/query spec
+                     [(format "select * from \"invTypes\" where \"typeName\" = '%s'" typeid)]))))
 
 (defn ship-typeid [name]
-  (:typeid
-   (first (sql/query spec
-                     [(format "select * from \"invTypes\" where \"typeName\" = '%s'" name)]))))
+  (:typeid (query-ship-info name)))
 
 (defn ship-info [typeid]
   (cond (instance? Number typeid) (query-ship-info typeid)
-        (instance? String typeid) (query-ship-info (ship-typeid typeid))))
+        (instance? String typeid) (query-ship-info typeid)))
