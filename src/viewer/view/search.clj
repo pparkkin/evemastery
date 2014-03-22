d8a6f29cfb8d5ea9b703679f8873c10657b84565
Paavo <pparkkin@gmail.com>
2014-03-22 15:31:10 +0900

    Search page styling.

--- src/viewer/view/search.clj
+++ src/viewer/view/search.clj
@@ -5,9 +5,11 @@
 (defn render []
   (page/html5
    [:head
+    (page/include-css "/stylesheets/search.css")
     [:title "Search for it."]]
    [:body
-    [:h1 "Search by ship name"]
-    [:form
-     [:input {:type "text" :name "q"}]
-     [:input {:type "submit" :value "Search"}]]]))
+    [:div#content
+     [:h1 "Search by ship name"]
+     [:form
+      [:input {:type "text" :name "q"}]
+      [:input {:type "submit" :value "Search"}]]]]))
