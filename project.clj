77d8f87f9964be4c52a51e86774caf4858d5e4ee
Paavo <pparkkin@gmail.com>
2014-03-23 20:58:52 +0900

    Bring back the Postgres libs.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0-RC1"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]
                  [ring "1.2.2"]
                  [compojure "1.1.6"]
                  [org.clojure/data.json "0.2.4"]
