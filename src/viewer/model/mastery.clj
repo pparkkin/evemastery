cea5c4f7779dfc4d4cfe582a8c54bb28c1b665ea
Paavo <pparkkin@gmail.com>
2014-03-25 21:05:30 +0900

    Only include a skill once in XML.

--- src/viewer/model/mastery.clj
+++ src/viewer/model/mastery.clj
@@ -5,6 +5,26 @@
 (defn roman-to-masterylevel [r]
   ({"I" 0 "II" 1 "III" 2 "IV" 3 "V" 4} r))
 
+(defn new-skill [id name level]
+  {:skilllevel level :typename name :typeid id})
+
+(defn new-skill-list []
+  {:s #{} :l []})
+
+(defn add-skill [list skill]
+  (if (contains? (:s list) skill)
+    list
+    {:s (conj (:s list) skill)
+     :l (conj (:l list) skill)}))
+
+(defn add-skills [list skills]
+  (reduce (fn [l s] (add-skill l s))
+          list
+          skills))
+
+(defn list-skills [list]
+  (:l list))
+
 ;; <entry skillID="3330" skill="Caldari Frigate" level="1"/>
 (defn skills-as-xml [ss]
   (map (fn [s] {:tag :entry
@@ -14,10 +34,11 @@
        ss))
 
 (defn masteries-as-xml [ms]
-  ;; Certs inside masteries, skills inside certs
-  (mapcat (fn [[l m]]
-            (mapcat (fn [c] (skills-as-xml (:skills c))) m))
-          ms))
+  (let [skills (reduce (fn [ss [l m]]
+                         (reduce (fn [sss c] (add-skills sss (:skills c))) ss m))
+                       (new-skill-list)
+                       ms)]
+    (skills-as-xml (list-skills skills))))
 
 ;; <plan xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" name="Condor - Medium" revision="4016">
 (defn as-xml [shipname level]
